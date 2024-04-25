c6948ba4325a816990f595657abee9784e1f62d8
user <mishakopytok@gmail.com>
2024-04-26 01:00:05 +0300

    init comit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module project1
+
+go 1.22.2
+
+require (
+	github.com/alehano/wsgame v0.0.0-20140518135246-23a644fc015c
+	github.com/gorilla/websocket v1.5.1
+)
+
+require golang.org/x/net v0.17.0 // indirect
---
+++ program.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/alehano/wsgame/game"
+	"log"
+	//"github.com/alehano/wsgame/game"
+	"github.com/gorilla/websocket"
+	"net/http"
+	"net/url"
+)
+
+var ADDR = ":8080"
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if _, ok := err.(websocket.HandshakeError); ok {
+		http.Error(w, "Not a websocket handshake", 400)
+		return
+	} else if err != nil {
+		return
+	}
+	playerName := "Player"
+	params, _ := url.ParseQuery(r.URL.RawQuery)
+	if len(params["name"]) > 0 {
+		playerName = params["name"][0]
+	}
+	// Get or create a room
+	var room *room
+	if len(freeRooms) > 0 {
+		for _, r := range freeRooms {
+			room = r
+			break
+		}
+	} else {
+		room = NewRoom("")
+	}
+	// Create Player and Conn
+	player := game.NewPlayer(playerName)
+	pConn := NewPlayerConn(ws, player, room)
+	// Join Player to room
+	room.join <- pConn
+	log.Printf("Player: %s has joined to room: %s", pConn.Name, room.name)
+}
+func main() {
+	http.HandleFunc("/ws", wsHandler)
+
+	if err := http.ListenAndServe(ADDR, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
